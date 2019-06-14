b4b78ce07708c207898315460b2ce772c197095c
Edouard Richard <vied12@gmail.com>
2019-06-14 10:45:54 +0200

    chore: remove tuner

--- src/looped/__init__.py
+++ src/looped/__init__.py
@@ -1,11 +1,10 @@
 from .Led import create_led
-from .animations import Jump, Tuner, Intro, Metronome
+from .animations import Jump, Intro
 from .gamepads import WebGamePad
 
 __all__ = [
     'create_led',
     'Jump',
     'WebGamePad',
-    'Tuner',
     'Intro',
 ]
